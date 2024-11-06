0bdb83dc8fb7384f9d5d941cd52743870e2f708f
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-11-07 00:41:46 +0100

    [Front] Home page

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Container, Row, Button} from 'reactstrap'
 import { NavLink, Link } from 'react-router-dom';
 
-import logo from '../../assets/images/logo_myloc_hori.png'
+import logo from '../../assets/images/logo_myloc_hori.png';
+import "./header.css";
 
 const nav__links=[
     {
@@ -10,7 +11,7 @@ const nav__links=[
         display:'Home'
     },
     {
-        path:'#',
+        path:'/about',
         display:'About'
     },
     {
@@ -27,6 +28,37 @@ const Header = () => {
             <div className="nav__wrapper d-flex align-items-center justify-content-between">
                 <div className="logo">
                     <img src={logo} alt="" />
+                </div>
+                
+                <div className='navigation'>
+                    <ul className="menu d-flex align-items-center gap-5">
+                        {nav__links.map((item, index) => (
+                            <li className="nav__item" key={index}>
+                                <NavLink
+                                 to={item.path} 
+                                 className={navClass =>
+                                    navClass.isActive ? "active__link" : ""
+                                }
+                                >
+                                    {item.display}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="nav__right d-flex align-items-center gap-4">
+                    <div className="nav__btns d-flex align-items-center gap-4">
+                        <Button className="btn secondary__btn">
+                            <Link to='/login'>Login</Link>
+                        </Button>
+                        <Button className="btn primary__btn">
+                            <Link to='/register'>Register</Link>
+                        </Button>
+                    </div>
+                    <span className="mobile__menu">
+                        <i class="ri-menu-line"></i>
+                    </span>
                 </div>
             </div>
         </Row>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import '../styles/home.css'
+
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import Subtitle from '../shared/Subtitle';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <>
+  <section>
+    <Container>
+      <Row>
+        <Col lg='6'>
+        <div className="hero__content">
+          <div className="hero__subtitle d-flex align-items-center ">
+            <Subtitle subtitle={'Compare Befor You Go'} />
+            <img src={worldImg} alt="" />
+          </div>
+          <h1>
+          Location de voiture, longue et courte durée{" "}
+            <span className="highlight">🚙</span>
+          </h1>
+          <p>
+          Explorez en style avec notre service de location de voiture à large gamme de choix ! 🌟
+          Réservez dès maintenant pour garantir la disponibilité du véhicule de votre choix !
+          </p>
+        </div>
+        </Col>
+      </Row>
+    </Container>
+  </section>
+  </>
 }
 
 export default Home
