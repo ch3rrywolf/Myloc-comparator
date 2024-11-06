a5a1c90038cd8f637fa942540cb27b08fb35a8ba
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-11-06 23:24:58 +0100

    [Front] Header Logo

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Button} from 'reactstrap'
+import { NavLink, Link } from 'react-router-dom';
+
+import logo from '../../assets/images/logo_myloc_hori.png'
+
+const nav__links=[
+    {
+        path:'/home',
+        display:'Home'
+    },
+    {
+        path:'#',
+        display:'About'
+    },
+    {
+        path:'/voitures',
+        display:'Voitures'
+    },
+]
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  return <header className="header">
+    <Container>
+        <Row>
+            <div className="nav__wrapper d-flex align-items-center justify-content-between">
+                <div className="logo">
+                    <img src={logo} alt="" />
+                </div>
+            </div>
+        </Row>
+    </Container>
+  </header> 
+};
 
 export default Header
