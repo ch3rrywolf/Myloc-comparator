cd2ddb171e334329b923d14a816f916a964c1d31
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-11-07 21:38:12 +0100

    [Front] Home VoitureCard Featured

--- frontend/src/shared/VoitureCard.jsx
+++ frontend/src/shared/VoitureCard.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Card, CardBody } from "reactstrap";
 import { Link } from "react-router-dom";
 import './voiture-card.css';
+import calculateAvgRating from "../utils/avgRating";
 
 const VoitureCard = ({ voiture }) => {
-    const { id, title, photo, city, price, featured, avgRating, reviews } = voiture;
+    const { id, title, photo, city, price, featured, reviews } = voiture;
+
+    const { totalRating, avgRating } = calculateAvgRating(reviews);
+    
   return (
     <div className="voiture__card">
         <Card>
             <div className="voiture__img">
                 <img src={photo} alt="voiture-img" />
-                <span>Featured</span>
+                {featured && <span>Featured</span>}
             </div>
 
             <CardBody>
@@ -19,11 +23,15 @@ const VoitureCard = ({ voiture }) => {
                     <i class="ri-map-pin-line"></i> {city}
                 </span>
                 <span className="voiture__location d-flex align-items-center gap-1">
-                    <i class="ri-star-fill"></i> {avgRating}{" "}
-                    <span>({reviews.length})</span>
+                    <i class="ri-star-fill"></i> {avgRating === 0 ? null : avgRating}
+                    {totalRating === 0 ? (
+                        "Not rated"
+                    ) : (
+                        <span>({reviews.length})</span>
+                    )}
                 </span>
             </div>
-            <h5 className="voiture__title"><Link to={`/voitures/${id}`}></Link></h5>
+            <h5 className="voiture__title"><Link to={`/voitures/${id}`}>{title}</Link></h5>
             <div className="card__bootom d-flex align-items-center justify-content-between mt-3">
                 <h5>${price}<span> /per person</span></h5>
                 <button className="btn booking__btn">
